23661e02b68fd5b380fa0cfa2e5241b2f611dd0a
Carter-A <carter.s.andrew@gmail.com>
2015-10-30 18:07:47 -0400

    final commit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,15 +1,11 @@
-def vowel?(str)
-  ["a","e","i","o","u"].include?(str)
-end
-
-def translate_word(word)
-  first_vowel_idx = word.chars.find_index{|c| vowel?(c)}
-  leading_consonants = word[0..first_vowel_idx]
-  rest_of_the_word = word[first_vowel_idx..-1]
-  rest_of_the_word + leading_consonants + 'ay'
-end
-
-def translate(sentence)
-  words = sentence.split(" ")
-  words.map{|w| translate_word(w) }.join(" ")
+def translate (sent)
+  vowels = %w{a e i o u}
+  sent.gsub(/(\A|\s)\w+/) do |str|
+    str.strip!
+    while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+      str += str[0]
+      str = str[1..-1]
+    end
+    str  = ' ' + str + 'ay'
+  end.strip
 end
